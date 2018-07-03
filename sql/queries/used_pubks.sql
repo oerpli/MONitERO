d3c6b7d500ef39fbf6fe078953cddd50462d259d
Abraham Hinteregger <oerpli@hotmail.com>
2018-07-03 17:46:21 +0200

    removed deprecated query (TODO: remove others as well)

--- sql/queries/used_pubks.sql
+++
@@ -1,8 +0,0 @@
-\timing on
-
-\set name spent_pubks
-\set file :outfolder:name'.csv'''
-COPY (SELECT pubk FROM txout natural join ring where matched = 'real' or matched ='spent') TO :file CSV HEADER DELIMITER E'\t';
-
-COMMENT ON TABLE :name IS 'Query: '; -- PLEASE FILL OUT TO PREVENT CONFUSION
-
