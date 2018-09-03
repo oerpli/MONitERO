9516c256935ce1b4762f90594cf09e2577f73fb5
Abraham Hinteregger <oerpli@hotmail.com>
2018-09-03 12:18:47 +0200

    cleaned up fork exports

--- tx_exporter/xmo_export.sh
+++ tx_exporter/xmo_export.sh
@@ -5,7 +5,6 @@ mkdir xmo_data
 cd xmo_data
 rm * # beware!
 
-../../xmr-exporter/xmv2csv -b ../../../../monero-data/xmo-data/lmdb -t $start --all-key-images --all-outputs --out-csv-file4 "inputs.csv"
+../../tx_exporter/xmv2csv -b ../../../monero-data/xmo-data/lmdb -t $start --all-key-images --all-outputs --out-csv-file4 "inputs.csv"
 
 mv xmr_report.csv outputs.csv
-
--- tx_exporter/xmv_export.sh
+++ tx_exporter/xmv_export.sh
@@ -1,13 +1,10 @@
 start=1564965
-to=$2
-count=$((start - to))
 
 cd ../data/
 mkdir xmv_data
 cd xmv_data
 rm * # beware!
 
-../../xmr-exporter/xmv2csv -b ../../../../monero-data/xmv-data/lmdb -t $start --all-key-images --all-outputs --out-csv-file4 "inputs.csv"
+../../tx_exporter/xmv2csv -b ../../../monero-data/xmv-data/lmdb -t $start --all-key-images --all-outputs --out-csv-file4 "inputs.csv"
 
 mv xmr_report.csv outputs.csv
-
